666ce29e04d80e0a7261221707483b49b37082ba
Sad228 <mov4ko@gmail.com>
2023-04-26 15:58:58 +0300

    lesson1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import './App.css';
+import Simpsons from "./components/simpsons/simpsons";
+import Characters from "./components/Characters/Characters";
 
 function App() {
   return (
-    <div>
+      <div >
+          <Simpsons/>
+          <Characters/>
 
-    </div>
+      </div>
   );
 }
 
